f91d4068059fd33b03301fb061014d6192d7116d
Peter Odell <odellpm@yahoo.com>
2014-04-23 20:55:28 -0700

    servo initialization

--- arduino/bluetooth/bluetooth.ino
+++ arduino/bluetooth/bluetooth.ino
@@ -1,7 +1,17 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
+
+Servo servo1;
+
+//brushless esc wire 
+//orange = signal
+//red = plus
+//brown is ground
 
 const int TX_BT = 10;
 const int RX_BT = 11;
+
+const int signalPin = 3;
 
 const int PIN_RED = 5;
 const int PIN_GREEN = 6;
@@ -20,22 +30,7 @@ void processCommand(char* command) {
   if(strcmp(command,"TURN_ON_RED") == 0) {
     digitalWrite(PIN_RED, HIGH);
     sendMessage("LED_RED:ON");
-  } else if(strcmp(command,"TURN_OFF_RED") == 0) {
-    digitalWrite(PIN_RED, LOW);
-    sendMessage("LED_RED:OFF");
-  } else if(strcmp(command,"TURN_ON_GREEN") == 0) {
-    digitalWrite(PIN_GREEN, HIGH);
-    sendMessage("LED_GREEN:ON");
-  } else if(strcmp(command,"TURN_OFF_GREEN") == 0) {
-    digitalWrite(PIN_GREEN, LOW);
-    sendMessage("LED_GREEN:OFF");
-  } else if(strcmp(command,"TURN_ON_YELLOW") == 0) {
-    digitalWrite(PIN_YELLOW, HIGH);
-    sendMessage("LED_YELLOW:ON");
-  } else if(strcmp(command,"TURN_OFF_YELLOW") == 0) {
-    digitalWrite(PIN_YELLOW, LOW);
-    sendMessage("LED_YELLOW:OFF");    
-  }
+  } 
 }
 
 //Send a message back to the Windows Phone.
@@ -51,11 +46,6 @@ void sendMessage(char* message) {
 //This message could be a sensor data, like a thermometer data.
 void sendPeriodicMessages() {
   int valProximitySensor = digitalRead(PIN_PROXIMITY_SENSOR);
-  if(valProximitySensor == HIGH) {
-    sendMessage("PROXIMITY:NOT_DETECTED");
-  } else {
-    sendMessage("PROXIMITY:DETECTED");
-  }
 }
 
 //Setup Arduino function
@@ -64,11 +54,9 @@ void setup() {
   Serial.println("USB Connected");
   btSerial.begin(9600);
   
-  pinMode(PIN_RED, OUTPUT);
-  pinMode(PIN_GREEN, OUTPUT);
-  pinMode(PIN_YELLOW, OUTPUT);  
+
   
-  pinMode(PIN_PROXIMITY_SENSOR, INPUT);
+  pinMode(signalPin, OUTPUT);
 }
 
 //Loop Arduino function
