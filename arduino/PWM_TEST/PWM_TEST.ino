946d0f7255bdd66dfdf6a762743957e0e28bba9b
noah hamm <phamh1987@gmail.com>
2014-04-24 07:53:13 -0700

    Touched PWM_TEST

--- arduino/PWM_TEST/PWM_TEST.ino
+++ arduino/PWM_TEST/PWM_TEST.ino
@@ -3,6 +3,16 @@
 #include <SoftwareSerial.h>
 
 Servo servo1;
+
+//brushless esc wire 
+//orange = signal
+//red = plus
+//brown is ground
+
+const int TX_BT = 10;
+const int RX_BT = 11;
+
+
 int test = 5;
 int i=0;
  
@@ -14,21 +24,38 @@ int throttle = 0;
 int cmd=0; 
 String echo="";
 
+SoftwareSerial btSerial(TX_BT,RX_BT);
+
 void setup() {
+  
+  //initialize serial comm through USB
   Serial.begin(9600);
   Serial.println("USB Connected");
-  // put your setup code here, to run once:
-  pinMode(test, OUTPUT);
+  
+  //initialize serial com through bluetooth
+  btSerial.begin(9600);
+  
+  //servo init
+  //attach dedicate pin to servo
   servo1.attach(3);
-  digitalWrite(test, HIGH);
+  ///////////////////////////////
+
   
 }
 
 
 void loop() {
+  /*
+  //read bluetooth serial buffer
+  if(btSerial.available()){
+    cmd=btSerial.parseInt();
+    Serial.println(cmd);    
+  }
+  */
     // Read serial input:
   if(Serial.available()){
-   cmd=Serial.parseInt();
+    cmd=Serial.parseInt();
+    servo1.write(cmd);
    
     Serial.println(cmd); 
   }
