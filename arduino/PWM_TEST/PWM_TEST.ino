584b519136f2bba02cefef4d6c8ef92f6b2be0f0
peatear <odellpm@yahoo.com>
2014-04-26 14:40:01 -0700

    Reads from bluetooth and outputs to motor

--- arduino/PWM_TEST/PWM_TEST.ino
+++ arduino/PWM_TEST/PWM_TEST.ino
@@ -1,13 +1,14 @@
-//hello
 #include <Servo.h>
 #include <SoftwareSerial.h>
 
 Servo servo1;
 
-//brushless esc wire 
-//orange = signal
-//red = plus
-//brown is ground
+/********************
+brushless esc wire 
+orange = signal
+red = plus
+brown is ground
+********************/
 
 const int TX_BT = 10;
 const int RX_BT = 11;
@@ -15,13 +16,14 @@ const int RX_BT = 11;
 
 int test = 5;
 int i=0;
+int cmdOut = 0;
  
 int outPWM = 9;
 int duty = 0; //duty cycle
 int serialRead = 0;  
 int throttle = 0;
 
-int cmd=0; 
+float cmd=0; 
 String echo="";
 
 SoftwareSerial btSerial(TX_BT,RX_BT);
@@ -39,26 +41,45 @@ void setup() {
   //attach dedicate pin to servo
   servo1.attach(3);
   ///////////////////////////////
-
+  servo1.writeMicroseconds(2500);
+  delay(2000);
+  //servo1.writeMicroseconds(2500);
+  servo1.writeMicroseconds(1000); 
+  //delay(3000);
+  //digitalWrite(test, LOW);
+  //for(int i = 0; i<178; i++) {
+  //    servo1.write(i);
+  //    delay(50);
+  //}
+  //servo1.write(0);
+  //delay(3000);
+ // for(int i = 178; i>1; i--) {
+//      servo1.write(i);
+ //     delay(50);
+ // }
   
 }
 
 
 void loop() {
-  /*
+  
   //read bluetooth serial buffer
   if(btSerial.available()){
-    cmd=btSerial.parseInt();
-    Serial.println(cmd);    
+    cmd=btSerial.parseFloat();
+    //cmd=(int)btSerial.read();
+    //Serial.write(cmd);
+     
+    cmdOut = map(cmd, -90, 90, 1000, 1200);
+    Serial.println(cmdOut);  
+    servo1.writeMicroseconds(cmdOut); 
   }
-  */
+/*
     // Read serial input:
   if(Serial.available()){
     cmd=Serial.parseInt();
-    servo1.write(cmd);
+    servo1.writeMicroseconds(cmd);
    
     Serial.println(cmd); 
-  }
+  }*/
 }
-
 
